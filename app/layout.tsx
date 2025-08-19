1b09c256fb16da4c4df5fe9a796846cbba940da0
inqodo <musamasays@gmail.com>
2025-08-19 17:26:01 +0700

    analytics asdded

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -26,7 +27,33 @@ export default function RootLayout({
       afterSignUpUrl="/dashboard"
     >
       <html lang="en">
-        <body className={`${inter.className} bg-gradient-to-br from-rose-50 to-pink-50 min-h-screen`}>{children}</body>
+        <head>
+          {/* Google Tag Manager */}
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T98ZDPS7');
+            `}
+          </Script>
+        </head>
+        <body
+          className={`${inter.className} bg-gradient-to-br from-rose-50 to-pink-50 min-h-screen`}
+        >
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-T98ZDPS7"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
